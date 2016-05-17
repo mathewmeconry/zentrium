e6fe8df39f34adaf076616f9f5e796e8a8d510d0
vkaf <webmaster@vkaf.ch>
2016-05-17 19:43:41 +0200

    Use install-workspace-deps to install NPM packages for all bundles

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,20 @@
 module.exports = function(grunt) {
   grunt.initConfig({
+    'install-workspace-deps': {
+      options: {
+        modules: ['src/**/*Bundle/Resources'],
+      },
+    },
     hub: {
       all: {
         src: ['src/**/*Bundle/Resources/Gruntfile.js'],
       },
     },
   });
+
+  grunt.loadNpmTasks('grunt-install-workspace-deps');
   grunt.loadNpmTasks('grunt-hub');
+
+  grunt.registerTask('install', ['install-workspace-deps']);
   grunt.registerTask('default', ['hub:all:default']);
 };
