9fb50a99b6de08235cfd2ba6202c73bc3736c144
vkaf <webmaster@vkaf.ch>
2016-05-17 13:41:30 +0200

    [Log] Add dashboard widget

---
+++ src/Zentrium/Bundle/LogBundle/Dashboard/DashboardListener.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Zentrium\Bundle\LogBundle\Dashboard;
+
+use Symfony\Component\Templating\EngineInterface;
+use Zentrium\Bundle\CoreBundle\Dashboard\BuildDashboardEvent;
+use Zentrium\Bundle\LogBundle\Entity\Log;
+use Zentrium\Bundle\LogBundle\Entity\LogRepository;
+
+class DashboardListener
+{
+    private $engine;
+    private $logRepository;
+
+    public function __construct(EngineInterface $engine, LogRepository $logRepository)
+    {
+        $this->engine = $engine;
+        $this->logRepository = $logRepository;
+    }
+
+    public function onBuildDashboard(BuildDashboardEvent $event)
+    {
+        $stats = array_merge([Log::STATUS_OPEN => 0], $this->logRepository->aggregateByStatus());
+        $logWidget = $this->engine->render(
+            'ZentriumLogBundle:Dashboard:logWidget.html.twig',
+            ['count' => $stats[Log::STATUS_OPEN]]
+        );
+        $event->addWidget(BuildDashboardEvent::POSITION_TOP, $logWidget);
+    }
+}
