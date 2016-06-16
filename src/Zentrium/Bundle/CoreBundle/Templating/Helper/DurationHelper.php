a9d490c933260480ba769445eb270fcc73f974d8
vkaf <webmaster@vkaf.ch>
2016-06-16 15:20:45 +0200

    [Core] Extend formatting options for durations

--- src/Zentrium/Bundle/CoreBundle/Templating/Helper/DurationHelper.php
+++ src/Zentrium/Bundle/CoreBundle/Templating/Helper/DurationHelper.php
@@ -21,10 +21,11 @@ class DurationHelper
      * Formats a duration in a human-readable manner.
      *
      * @param Period|int $duration
+     * @param array      $options
      *
      * @return string
      */
-    public function format($duration)
+    public function format($duration, $options = [])
     {
         if ($duration instanceof Period) {
             $duration = $duration->getTimestampInterval();
@@ -33,11 +34,24 @@ class DurationHelper
             return $duration;
         }
 
+        $options = array_merge([
+            'format' => null,
+            'with_seconds' => null,
+        ], $options);
+
         $seconds = abs(round($duration));
         $hours = floor($seconds / 3600);
         $seconds -= $hours * 3600;
         $minutes = floor($seconds / 60);
         $seconds -= $minutes * 60;
+
+        if ($options['format'] === 'compact') {
+            if ($options['with_seconds'] === true || ($options['with_seconds'] === null && $seconds > 0)) {
+                return ($duration < 0 ? '-' : '').sprintf('%d:%02d:%02d', $hours, $minutes, $seconds);
+            } else {
+                return ($duration < 0 ? '-' : '').sprintf('%d:%02d', $hours, $minutes);
+            }
+        }
 
         $parts = [];
         if ($hours > 0) {
--- tests/Zentrium/Bundle/CoreBundle/Tests/Templating/Helper/DurationHelperTest.php
+++ tests/Zentrium/Bundle/CoreBundle/Tests/Templating/Helper/DurationHelperTest.php
@@ -24,31 +24,54 @@ class DurationHelperTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * @dataProvider provideSamples
+     * @dataProvider provideFormatSamples
      */
-    public function testFormat($duration, $expectedOutput)
+    public function testFormat($duration, $expected, $expectedCompact)
     {
         $helper = new DurationHelper($this->translator);
 
-        $this->assertSame($expectedOutput, $helper->format($duration));
+        $this->assertSame($expected, $helper->format($duration));
+        $this->assertSame($expectedCompact, $helper->format($duration, ['format' => 'compact']));
     }
 
-    public function provideSamples()
+    public function provideFormatSamples()
     {
         return [
-            [new Period(new DateTime('2016-01-01 00:00:00'), new DateTime('2016-01-03 00:00:10')), '48 h 10 s'],
-            [600, '10 m'],
-            [-600, '- 10 m'],
-            [72 * 3600, '72 h'],
-            [-72 * 3600, '- 72 h'],
-            [0, '0 s'],
-            [1, '1 s'],
-            [-1, '- 1 s'],
-            [120.2, '2 m'],
-            [120.8, '2 m 1 s'],
-            [-120.2, '- 2 m'],
-            [-120.8, '- 2 m 1 s'],
-            [null, null],
+            [new Period(new DateTime('2016-01-01 00:00:00'), new DateTime('2016-01-03 00:00:10')), '48 h 10 s', '48:00:10'],
+            [600, '10 m', '0:10'],
+            [-600, '- 10 m', '-0:10'],
+            [72 * 3600, '72 h', '72:00'],
+            [-72 * 3600, '- 72 h', '-72:00'],
+            [0, '0 s', '0:00'],
+            [1, '1 s', '0:00:01'],
+            [-1, '- 1 s', '-0:00:01'],
+            [120.2, '2 m', '0:02'],
+            [120.8, '2 m 1 s', '0:02:01'],
+            [-120.2, '- 2 m', '-0:02'],
+            [-120.8, '- 2 m 1 s', '-0:02:01'],
+            [null, null, null],
+        ];
+    }
+
+    /**
+     * @dataProvider provideFormatWithSecondsSamples
+     */
+    public function testFormatWithSeconds($duration, $withSeconds, $expected)
+    {
+        $helper = new DurationHelper($this->translator);
+
+        $this->assertSame($expected, $helper->format($duration, ['format' => 'compact', 'with_seconds' => $withSeconds]));
+    }
+
+    public function provideFormatWithSecondsSamples()
+    {
+        return [
+            [7200, true, '2:00:00'],
+            [7200, false, '2:00'],
+            [7200, null, '2:00'],
+            [7201, true, '2:00:01'],
+            [7201, false, '2:00'],
+            [7201, null, '2:00:01'],
         ];
     }
 }
