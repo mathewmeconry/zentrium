e92ef317b3b9159508b46c57c857f3b3b4734960
vkaf <webmaster@vkaf.ch>
2016-06-17 22:37:22 +0200

    [Schedule] Test that all builders load correctly

---
+++ tests/Zentrium/Bundle/ScheduleBundle/Tests/Controller/ConstraintControllerTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Zentrium\Bundle\ScheduleBundle\Tests\Controller;
+
+use Zentrium\Bundle\CoreBundle\Tests\WebTestCase;
+
+class ConstraintControllerTest extends WebTestCase
+{
+    public function testNew()
+    {
+        $this->loadAllFixtures();
+        $this->loginByReference('user-manager');
+        $client = static::makeClient();
+
+        $crawler = $client->request('GET', '/schedules/constraints/new');
+        $this->assertStatusCode(200, $client);
+
+        $typeLinks = $crawler->filter('.content li a')->each(function ($node) {
+            return $node->link();
+        });
+        $this->assertGreaterThanOrEqual(5, count($typeLinks));
+
+        foreach ($typeLinks as $typeLink) {
+            $client->click($typeLink);
+            $this->assertStatusCode(200, $client);
+        }
+    }
+}
