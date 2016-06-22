5b41af9ea735fdef1963aee61187d3581a01a2b2
vkaf <webmaster@vkaf.ch>
2016-06-22 14:17:08 +0200

    Remove invalid bundle from kernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -35,7 +35,6 @@ class AppKernel extends Kernel
             new Zentrium\Bundle\CoreBundle\ZentriumCoreBundle(),
             new Zentrium\Bundle\ScheduleBundle\ZentriumScheduleBundle(),
             new Zentrium\Bundle\TimesheetBundle\ZentriumTimesheetBundle(),
-            new Vkaf\Bundle\OperationBundle\VkafOperationBundle(),
             new Vkaf\Bundle\OafBundle\VkafOafBundle(),
         ];
 
