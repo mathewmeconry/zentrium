824ef9baa712633a29aea4ec694b18143db40a90
vkaf <webmaster@vkaf.ch>
2018-05-07 16:20:22 +0200

    [WIP] Add icons to notifications

--- src/Vkaf/Bundle/OafBundle/Resources/js/worker.js
+++ src/Vkaf/Bundle/OafBundle/Resources/js/worker.js
@@ -10,6 +10,7 @@ self.addEventListener('push', event => {
   if (Notification.permission != 'granted') {
     return;
   }
+
   const payload = event.data.json();
   if (!payload) {
     return;
@@ -17,11 +18,17 @@ self.addEventListener('push', event => {
   if (payload.url && new URL(payload.url).origin !== self.location.origin) {
     return;
   }
-  event.waitUntil(self.registration.showNotification(payload.title, {
+
+  const options = {
     body: payload.body || undefined,
     data: payload.url,
     tag: payload.tag || undefined,
-  }));
+    icon: '/icon-192x192.png',
+  };
+  if ('badge' in Notification.prototype) {
+    options.badge = '/badge-36x36.png';
+  }
+  event.waitUntil(self.registration.showNotification(payload.title, options));
 });
 
 self.addEventListener('notificationclick', event => {
