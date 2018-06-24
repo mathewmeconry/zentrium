6a873102d785c6591915f85423f7a181bff5a886
vkaf <webmaster@vkaf.ch>
2018-06-24 09:22:24 +0200

    [Core] Make searchable table reusable

--- src/Zentrium/Bundle/CoreBundle/Resources/js/layout.js
+++ src/Zentrium/Bundle/CoreBundle/Resources/js/layout.js
@@ -1,4 +1,5 @@
 import $ from 'jquery';
+import { Translator } from 'zentrium';
 
 $(function () {
   $('[data-original-title][data-toggle="dropdown"]').tooltip();
@@ -9,4 +10,30 @@ $(function () {
       e.stopImmediatePropagation();
     }
   }).tree();
+
+  $('.table-searchable').each(function () {
+    const $table = $(this);
+    const $inputRow = $('<tr><td><input class="form-control"></td></tr>');
+    const $input = $inputRow.find('input');
+    $table.children('thead').prepend($inputRow);
+    $input.attr('placeholder', Translator.trans('zentrium.search'))
+    $input.on('input', function () {
+      const query = $(this).val().toLowerCase().trim().split(/\s+/);
+      $table.children('tbody').children().each(function () {
+        const $row = $(this);
+        if (query.length === 1 && query[0] === '') {
+          $row.show();
+          return;
+        }
+        const text = $row.text().toLowerCase();
+        for (let token of query) {
+          if (text.indexOf(token) === -1) {
+            $row.hide();
+            return;
+          }
+        }
+        $row.show();
+      })
+    });
+  });
 });
